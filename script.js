ab097f2dc46c63a1b6c4a8f1aafd68389941ab0d
Your Name <you@example.com>
2022-06-03 22:59:01 +0530

    login and signup forms added

---
+++ script.js
@@ -0,0 +1,20 @@
+function openSignForm() {
+  document.getElementById("mySignForm").style.display = "block";
+   
+}
+
+function closeSignForm() {
+  document.getElementById("mySignForm").style.display = "none";
+ 
+}
+function openLogForm() {
+  document.getElementById("myLogForm").style.display = "block";
+   
+
+}
+
+function closeLogForm() {
+  document.getElementById("myLogForm").style.display = "none";
+  
+}
+
