4190a31c75c2860c62f5245ddc8a4284c5e1403b
Your Name <you@example.com>
2022-06-07 21:11:30 +0530

    form bg,login updated

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 function openSignForm() {
   document.getElementById("mySignForm").style.display = "block";
-   
+   document.querySelector(".signup-container").style.display = "block";
 }
 
 function closeSignForm() {
   document.getElementById("mySignForm").style.display = "none";
+ document.querySelector(".signup-container").style.display = "none";
  
 }
 function openLogForm() {
   document.getElementById("myLogForm").style.display = "block";
-   
+    document.querySelector(".login-container").style.display = "block";
 
 }
 
 function closeLogForm() {
   document.getElementById("myLogForm").style.display = "none";
-  
+  document.querySelector(".login-container").style.display = "none";
 }
 
